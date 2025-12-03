8473fdc39ac894f36a27eaa81164ad8fb3d8d4e4
AyoubHdn <ayoubhdn1@gmail.com>
2025-12-03 16:11:40 +0100

    now?

--- src/server/api/routers/s3.ts
+++ src/server/api/routers/s3.ts
@@ -38,8 +38,10 @@ export const s3Router = createTRPCRouter({
         Fields: {
           key,
           "Content-Type": input.filetype,
+          acl: "public-read"
         },
         Conditions: [
+          { acl: "public-read" },
           ["starts-with", "$Content-Type", ""],
           ["content-length-range", 0, 10 * 1024 * 1024],
         ],
