b842b0cf503ebcaabe40fbc5308852001a87fd9a
AyoubHdn <ayoubhdn1@gmail.com>
2025-12-03 22:04:32 +0100

    add cyberpunck style

--- src/data/twitchBannerStyles.ts
+++ src/data/twitchBannerStyles.ts
@@ -38,50 +38,89 @@ export interface TwitchBannerStyle {
 
 export const TWITCH_BANNER_STYLES: TwitchBannerStyle[] = [
   {
-    id: "blueModern1",
-    name: "Blue Modern Gamer",
-    previewSrc: "/twitch/banner/blue_modern-prv.png",
-    backgroundSrc: "/twitch/banner/blue_modern.webp",
-    creditCost: 2,
-    supportsPhoto: true,
-    category: "With Logo",
-    styleRules: {
-      canvasWidth: 1200,
-      canvasHeight: 480,
-
-      fonts: {
-        headline: { family: "Anton", file: "Anton-Regular.ttf" },
-        body: { family: "Anton", file: "Anton-Regular.ttf" }
+  id: "cyberpunkNeon",
+  name: "Cyberpunk Neon Frame",
+  previewSrc: "/twitch/banner/cyberpunk-prv.png",
+  backgroundSrc: "/twitch/banner/cyberpunk_bg.webp",
+  creditCost: 2,
+  supportsPhoto: true,
+  category: "With Logo",
+  styleRules: {
+    canvasWidth: 1200,
+    canvasHeight: 480,
+    photo: {
+      x: 830,
+      y: 60,
+      width: 270,
+      height: 270
+    },
+    fonts: {
+      headline: { family: "Anton", file: "Anton-Regular.ttf" },
+      body: { family: "Anton", file: "Anton-Regular.ttf" }
+    },
+    elements: {
+      channelName: {
+        x: 350,
+        y: 220,
+        fontSize: 120,
+        color: "#0ff",
+        fontFamily: "headline",
+        textAnchor: "start",
+        letterSpacing: 2
       },
-
-      elements: {
-        channelName: {
-          x: 360,
-          y: 200,
-          fontSize: 140,
-          color: "#FFFFFF",
-          fontFamily: "headline",
-          textAnchor: "start"
-        },
-        tagline: {
-          x: 360,
-          y: 280,
-          fontSize: 36,
-          color: "#FFFFFF",
-          fontFamily: "body",
-          textAnchor: "start"
-        }
-      },
-
-      photo: {
-        x: 750,
-        y: 30,
-        width: 360,
-        height: 420
+      tagline: {
+        x: 350,
+        y: 290,
+        fontSize: 32,
+        color: "#fff",
+        fontFamily: "body",
+        textAnchor: "start"
       }
     }
   }
-  ,
+},
+{
+  id: "cyberpunkNeon",
+  name: "Cyberpunk Neon Frame",
+  previewSrc: "/twitch/banner/cyberpunk-prv.png",
+  backgroundSrc: "/twitch/banner/cyberpunk_bg.webp",
+  creditCost: 2,
+  supportsPhoto: true,
+  category: "With Logo",
+  styleRules: {
+    canvasWidth: 1200,
+    canvasHeight: 480,
+    photo: {
+      x: 830,
+      y: 60,
+      width: 270,
+      height: 270
+    },
+    fonts: {
+      headline: { family: "Anton", file: "Anton-Regular.ttf" },
+      body: { family: "Anton", file: "Anton-Regular.ttf" }
+    },
+    elements: {
+      channelName: {
+        x: 350,
+        y: 220,
+        fontSize: 120,
+        color: "#0ff",
+        fontFamily: "headline",
+        textAnchor: "start",
+        letterSpacing: 2
+      },
+      tagline: {
+        x: 350,
+        y: 290,
+        fontSize: 32,
+        color: "#fff",
+        fontFamily: "body",
+        textAnchor: "start"
+      }
+    }
+  }
+},
 
   {
     id: "blueModern",
